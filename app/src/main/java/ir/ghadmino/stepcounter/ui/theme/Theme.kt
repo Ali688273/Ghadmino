2c84aedbebd8eba38f47c909be033d877bde16c7
Ali688273 <iiii688273@gmail.com>
2026-09-12 13:04:12 +0330

    Update Theme.kt

--- app/src/main/java/ir/ghadmino/stepcounter/ui/theme/Theme.kt
+++ app/src/main/java/ir/ghadmino/stepcounter/ui/theme/Theme.kt
@@ -1,54 +1,42 @@
 package ir.ghadmino.stepcounter.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val LightColors =
     lightColorScheme(
-
-        primary =
-            Color(0xFF176B5B),
-
-        secondary =
-            Color(0xFF4C635D),
-
-        tertiary =
-            Color(0xFF8A5000)
+        primary = Color(0xFF176B5B),
+        secondary = Color(0xFF4C635D),
+        tertiary = Color(0xFF8A5000),
+        background = Color(0xFFF8FAF9),
+        surface = Color(0xFFF8FAF9)
     )
 
 private val DarkColors =
     darkColorScheme(
-
-        primary =
-            Color(0xFF53DBC0),
-
-        secondary =
-            Color(0xFFB1CCC5),
-
-        tertiary =
-            Color(0xFFFFB95C)
+        primary = Color(0xFF53DBC0),
+        secondary = Color(0xFFB1CCC5),
+        tertiary = Color(0xFFFFB95C)
     )
 
 @Composable
 fun GhadminoTheme(
-
     darkTheme: Boolean =
         isSystemInDarkTheme(),
-
     content: @Composable () -> Unit
-
 ) {
 
     MaterialTheme(
-
         colorScheme =
-            if (darkTheme)
+            if (darkTheme) {
                 DarkColors
-            else
-                LightColors,
-
+            } else {
+                LightColors
+            },
         content = content
     )
 }
