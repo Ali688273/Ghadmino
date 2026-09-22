608048958fca1d352cea5c73e192f0f8dd6b0dc9
Ali688273 <iiii688273@gmail.com>
2026-09-22 11:55:50 +0330

    Fix debug build by removing unused Room KAPT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -95,18 +94,6 @@ dependencies {
 
     debugImplementation(
         "androidx.compose.ui:ui-tooling"
-    )
-
-    implementation(
-        "androidx.room:room-runtime:2.6.1"
-    )
-
-    implementation(
-        "androidx.room:room-ktx:2.6.1"
-    )
-
-    kapt(
-        "androidx.room:room-compiler:2.6.1"
     )
 
     implementation(
